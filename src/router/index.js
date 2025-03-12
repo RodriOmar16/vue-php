21aeb137ccdf8b907ebdd4e5999843145a0bf859
RodriOmar16 <rodrigoomarmiranda1@gmail.com>
2025-03-12 18:13:28 -0300

    12032025_R1

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'Home',
+      component: () => import('../views/general/Inicio.vue'),
+    },
+    {
       path: '/home',
       name: 'Inicio',
       component: () => import('../views/general/Inicio.vue'),
---
+++ src/views/general/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <div style="margin-top: 70px;">
+    Error 404. Página no encontrada
+  </div>
+</template>
+
+<script setup>
+  
+</script>
